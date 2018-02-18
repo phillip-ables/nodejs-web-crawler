aca263ad9d061faf05028f524224a4c5a4bca5a5
Phillip <phillipables1@gmail.com>
2018-02-17 22:05:40 -0500

    install and require cli-table so i dont have to host a web server and can display my results in the console

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const rp = require('request-promise');
 //allows syntax similar to jquery inside of node
 const cherio = require('cheerio');
+//easier to format our information, just displays the results right in the console
+const Table = require('cli-table');
