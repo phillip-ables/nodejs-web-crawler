732df4fee7c8ddc2cb3588768ba9165bc81b945e
Phillip <phillipables1@gmail.com>
2018-02-17 22:33:34 -0500

    set up options and called request promist parsed that data for eache user and called a function to get challanges completed and add to user array

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const rp = require('request-promise');
 const cherio = require('cheerio');
 //easier to format our information, just displays the results right in the console
 const Table = require('cli-table');
+//set some variables
+let user = [];
+//options for the request promise
+const options = {
+  url: "https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_recieved&_=1518604435748"
+  json: true
+}
+
+rp(options)
+  .then(data) => {
+    let userData = [];
+    for (let user of data.directory_items) {
+      userData.push({name: user.user.username, likes_recieved: user.likes_recieved})
+    }
+    process.stdout.write('loading');
+    getChallangesCompletedAndPushToUserArray(userData);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+function getChallangesCompletedAndPushToUserArray(userData) {
+  var i = 0;
+  function next() {
+    if(i < userData.length) {
+      var options = {
+        url: "https://www.freecode.com.org/"+ userData[i].name,
+        transform: body => cheerio.load(body)
+      }
+      rp(options)
+        .then(function ($) {
+          process.stout.write('.');
+
+        })
+    }
+  }
+}
